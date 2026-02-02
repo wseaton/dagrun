5d364e6dd545e15e60373aa94d9fae6bcfc623da
Will Eaton <weaton@redhat.com>
2026-02-01 20:31:42 -0500

    keep renaming stuff
    Signed-off-by: Will Eaton <weaton@redhat.com>

--- tree-sitter-dr/nvim-integration.lua
+++ tree-sitter-dr/nvim-integration.lua
@@ -1,11 +1,12 @@
--- nvim-treesitter integration for dagrun
+-- nvim-treesitter integration for dr
 -- WARNING: treesitter is currently disabled due to crashes on malformed syntax
 -- just register the filetype for now, enable treesitter at your own risk
 
 -- register filetype (always safe)
 vim.filetype.add({
   extension = {
-    dagrun = "dagrun",
+    dr = "dr",
+    dagrun = "dr", -- legacy extension support
   },
 })
 
@@ -13,27 +14,27 @@ vim.filetype.add({
 
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
-parser_config.dagrun = {
+parser_config.dr = {
   install_info = {
     -- point to local path or github url
-    url = "~/git/justflow/tree-sitter-dagrun", -- or "https://github.com/your-org/tree-sitter-dagrun"
+    url = "~/git/justflow/tree-sitter-dr", -- or "https://github.com/your-org/tree-sitter-dr"
     files = { "src/parser.c", "src/scanner.c" },
     branch = "main",
     generate_requires_npm = true,
   },
-  filetype = "dagrun",
+  filetype = "dr",
 }
 
 -- ensure queries are found (copy queries/ to your nvim runtime or symlink)
 -- option 1: symlink queries to nvim config
---   ln -s ~/git/justflow/tree-sitter-dagrun/queries ~/.config/nvim/queries/dagrun
+--   ln -s ~/git/justflow/tree-sitter-dr/queries ~/.config/nvim/queries/dr
 
 -- option 2: add to runtimepath
-vim.opt.runtimepath:append("~/git/justflow/tree-sitter-dagrun")
+vim.opt.runtimepath:append("~/git/justflow/tree-sitter-dr")
 
 -- then install the parser
--- :TSInstall dagrun
+-- :TSInstall dr
 -- or
--- :TSInstallFromGrammar dagrun
+-- :TSInstallFromGrammar dr
 
 ]]
